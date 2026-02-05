c59774ea59a280fb3efebeee3a38b152daa94260
sieunhok1 <nguyenhongson25121989@gmail.com>
2026-02-06 05:03:41 +0700

    tao giao dien signup

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,5 +1,13 @@
+import { SignupForm } from "../components/signup-form";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <SignupForm />
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;

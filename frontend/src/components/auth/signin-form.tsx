fe82a3ff8aabc0c9a949145f73f39979061b6d32
sieunhok1 <nguyenhongson25121989@gmail.com>
2026-02-06 05:43:29 +0700

    tao giao dien signUp va signIn

---
+++ frontend/src/components/auth/signin-form.tsx
@@ -0,0 +1,101 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const signinSchema = z.object({
+  username: z.string().min(3, "Tên đăng nhập phải có ít nhất 3 ký tự"),
+  password: z.string().min(6, "Mật khẩu phải có ít nhất 6 ký tự"),
+});
+
+type SigninpFormValues = z.infer<typeof signinSchema>;
+
+export function SigninForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SigninpFormValues>({ resolver: zodResolver(signinSchema) });
+
+  const onSubmit = async (data: SigninpFormValues) => {};
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0 border-border">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Chào mừng quay lại</h1>
+                <p className="text-muted-foreground text-sm text-balance">
+                  Đăng nhập vào tài khoản Moji của bạn
+                </p>
+              </div>
+
+              <Field>
+                <FieldLabel htmlFor="username">Tên đăng nhập</FieldLabel>
+                <Input
+                  id="username"
+                  type="text"
+                  required
+                  {...register("username")}
+                />
+                {errors.username && (
+                  <p className="text-destructive text-sm">
+                    {errors.username.message}
+                  </p>
+                )}
+              </Field>
+
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <p className="text-destructive text-sm">
+                    {errors.password.message}
+                  </p>
+                )}
+              </Field>
+              <Field>
+                <Button type="submit" disabled={isSubmitting}>
+                  Đăng nhập
+                </Button>
+              </Field>
+
+              <FieldDescription className="text-center">
+                Chưa có tài khoản? <a href="/signup">Đăng ký</a>
+              </FieldDescription>
+            </FieldGroup>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="/placeholderSignUp.png"
+              alt="Image"
+              className="absolute top-1/2 -translate-y-1/2 object-cover "
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center">
+        Bằng cách tiếp tục, bạn đồng ý với <a href="#">Điều khoản dịch vụ</a> và{" "}
+        <a href="#">Chính sách bảo mật</a>.
+      </FieldDescription>
+    </div>
+  );
+}
---
+++ frontend/src/components/auth/signup-form.tsx
@@ -0,0 +1,146 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const signUpSchema = z.object({
+  lastname: z.string().min(1, "Họ không được để trống"),
+  firstname: z.string().min(1, "Tên không được để trống"),
+  username: z.string().min(3, "Tên đăng nhập phải có ít nhất 3 ký tự"),
+  email: z.email("Email không hợp lệ"),
+  password: z.string().min(6, "Mật khẩu phải có ít nhất 6 ký tự"),
+});
+
+type SignUpFormValues = z.infer<typeof signUpSchema>;
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormValues>({ resolver: zodResolver(signUpSchema) });
+
+  const onSubmit = async (data: SignUpFormValues) => {};
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0 border-border">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Tạo tài khoản Moji</h1>
+                <p className="text-muted-foreground text-sm text-balance">
+                  Chào mừng bạn! Hãy đăng ký để bắt đầu
+                </p>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <Field>
+                  <FieldLabel htmlFor="lastname">Họ</FieldLabel>
+                  <Input
+                    id="lastname"
+                    type="text"
+                    required
+                    {...register("lastname")}
+                  />
+                  {errors.lastname && (
+                    <p className="text-destructive text-sm">
+                      {errors.lastname.message}
+                    </p>
+                  )}
+                </Field>
+                <Field>
+                  <FieldLabel htmlFor="firstname">Tên</FieldLabel>
+                  <Input
+                    id="firstname"
+                    type="text"
+                    required
+                    {...register("firstname")}
+                  />
+                  {errors.firstname && (
+                    <p className="text-destructive text-sm">
+                      {errors.firstname.message}
+                    </p>
+                  )}
+                </Field>
+              </div>
+              <Field>
+                <FieldLabel htmlFor="username">Tên đăng nhập</FieldLabel>
+                <Input
+                  id="username"
+                  type="text"
+                  required
+                  {...register("username")}
+                />
+                {errors.username && (
+                  <p className="text-destructive text-sm">
+                    {errors.username.message}
+                  </p>
+                )}
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  required
+                  {...register("email")}
+                />
+                {errors.email && (
+                  <p className="text-destructive text-sm">
+                    {errors.email.message}
+                  </p>
+                )}
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <p className="text-destructive text-sm">
+                    {errors.password.message}
+                  </p>
+                )}
+              </Field>
+              <Field>
+                <Button type="submit" disabled={isSubmitting}>
+                  Tạo tài khoản
+                </Button>
+              </Field>
+
+              <FieldDescription className="text-center">
+                Đã có tài khoản? <a href="/signin">Đăng nhập</a>
+              </FieldDescription>
+            </FieldGroup>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="/placeholderSignUp.png"
+              alt="Image"
+              className="absolute top-1/2 -translate-y-1/2 object-cover "
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center">
+        Bằng cách tiếp tục, bạn đồng ý với <a href="#">Điều khoản dịch vụ</a> và{" "}
+        <a href="#">Chính sách bảo mật</a>.
+      </FieldDescription>
+    </div>
+  );
+}
